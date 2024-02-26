5e9ffbf7d3c6bce54e3994b06ef9f7bdb98f2e39
Witchs-Cat <da18.06.03@yandex.ru>
2024-02-26 10:05:34 +0300

    creating HTTP-Server with FastAPI

---
+++ main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello" : "world"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q} 
+
+
+@app.get("/group/{id}")
+def read_groupId(id: int):
+    return {
+        "id": id
+        }
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080)
